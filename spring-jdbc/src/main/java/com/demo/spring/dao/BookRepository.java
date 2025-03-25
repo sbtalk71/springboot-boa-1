f942051104fc32e6794339eb6ff06fdd87342c86
sbtalk71 <sbtalk@yahoo.com>
2025-03-25 10:35:24 +0530

    spring jdbc commit 3

--- spring-jdbc/src/main/java/com/demo/spring/dao/BookRepository.java
+++ spring-jdbc/src/main/java/com/demo/spring/dao/BookRepository.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
@@ -14,36 +16,46 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookRepository implements MyJdbcRepository {
 	private JdbcTemplate jdbcTemplate;
-	
-	
+
 	public BookRepository(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	@Override
 	public List<Book> findAll() {
-		List<Book> booksList=jdbcTemplate.query(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				PreparedStatement pst=con.prepareStatement("select * from book");
-				return pst;
-			}
-		}, new RowMapper<Book>() {
-			@Override
-			public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-				
-				return new Book(rs.getString("isbn"), 
-						rs.getString("name"), rs.getString("author"), rs.getString("genre"));
-			}
-		});
+		/*
+		 * List<Book> booksList=jdbcTemplate.query(new PreparedStatementCreator() {
+		 * 
+		 * @Override public PreparedStatement createPreparedStatement(Connection con)
+		 * throws SQLException { PreparedStatement
+		 * pst=con.prepareStatement("select * from book"); return pst; } }, new
+		 * RowMapper<Book>() {
+		 * 
+		 * @Override public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		 * 
+		 * return new Book(rs.getString("isbn"), rs.getString("name"),
+		 * rs.getString("author"), rs.getString("genre")); } });
+		 */
+
+		List<Book> booksList = jdbcTemplate.query("select * from book", new BookRowMapper());
 		return booksList;
 	}
 
 	@Override
-	public Book save(Book book) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<Book> save(Book book) {
+
+		if (this.findById(book.getIsbn()).isPresent()) {
+			throw new BookExistsException("Book exists..");
+		} else {
+			int count = jdbcTemplate.update("insert into book(isbn,name,author,genre) values (?,?,?,?)", book.getIsbn(),
+					book.getName(), book.getAuthor(), book.getGenre());
+			if (count == 1) {
+				return findById(book.getIsbn());
+			} else {
+				throw new RuntimeException("Error");
+			}
+		}
+
 	}
 
 	@Override
@@ -59,9 +71,24 @@ public class BookRepository implements MyJdbcRepository {
 	}
 
 	@Override
-	public Book findById(String isbn) {
-		// TODO Auto-generated method stub
-		return null;
+	public Optional<Book> findById(String isbn) {
+		// Book book=new Book("0000","not available","NA","NA");
+		try {
+			Book book = jdbcTemplate.queryForObject("select * from book where isbn=?", new BookRowMapper(), isbn);
+			return Optional.of(book);
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+			return Optional.empty();
+		}
+
 	}
 
+}
+
+class BookRowMapper implements RowMapper<Book> {
+
+	@Override
+	public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return new Book(rs.getString("isbn"), rs.getString("name"), rs.getString("author"), rs.getString("genre"));
+	}
 }
--- spring-jdbc/src/main/java/com/demo/spring/dao/MyJdbcRepository.java
+++ spring-jdbc/src/main/java/com/demo/spring/dao/MyJdbcRepository.java
@@ -1,13 +1,14 @@
 package com.demo.spring.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MyJdbcRepository {
 
 	public List<Book> findAll();
-	public Book save(Book book);
+	public Optional<Book> save(Book book);
 	public Book update(Book book);
 	public void delete(String isbn);
-	public Book findById(String isbn);
+	public Optional<Book> findById(String isbn);
 	
 }
