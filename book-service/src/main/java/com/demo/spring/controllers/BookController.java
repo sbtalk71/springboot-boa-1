38936b9c556823a817df038064b6009b18faf19f
sbtalk71 <sbtalk@yahoo.com>
2025-03-26 11:46:59 +0530

    book service commit 6

--- book-service/src/main/java/com/demo/spring/controllers/BookController.java
+++ book-service/src/main/java/com/demo/spring/controllers/BookController.java
@@ -6,15 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.spring.BookService;
 import com.demo.spring.entities.Book;
+import com.demo.spring.service.BookService;
+import com.demo.spring.util.ResponseMessage;
 
 @RestController
 @RequestMapping("/books")
@@ -42,6 +45,18 @@ public class BookController {
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Book> addBook(@RequestBody Book book) {
 		return ResponseEntity.ok(bookService.saveBook(book));
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Book> updateTheBook(@RequestBody Book book) {
+		return ResponseEntity.ok(bookService.updateBook(book));
+	}
+	
+	
+	
+	@DeleteMapping(path="/{isbn}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseMessage> deleteTheBook(String isbn){
+		return ResponseEntity.ok(new ResponseMessage(bookService.deleteBook(isbn)));
 	}
 	
 }
--- book-service/src/main/java/com/demo/spring/service/BookService.java
+++ book-service/src/main/java/com/demo/spring/service/BookService.java
@@ -1,8 +1,10 @@
-package com.demo.spring;
+package com.demo.spring.service;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.demo.spring.entities.Book;
@@ -14,6 +16,8 @@ import com.demo.spring.repositories.BookRepository;
 public class BookService {
 
 	private BookRepository bookRepository;
+	
+	Logger logger=LoggerFactory.getLogger(this.getClass().getName());
 
 	public BookService(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
@@ -26,8 +30,10 @@ public class BookService {
 	public Book findBookById(String isbn) {
 		Optional<Book> bookOp = bookRepository.findById(isbn);
 		if (bookOp.isPresent()) {
+			logger.info("book found, returning to the controller");
 			return bookOp.get();
 		} else {
+			logger.info("book not found, throwing exception");
 			throw new BookNotFoundException("Book with ISBN " + isbn + " not found");
 		}
 	}
@@ -38,6 +44,23 @@ public class BookService {
 			throw new BookExistsException("The book exists..");
 		} else {
 			return bookRepository.save(book);
+		}
+	}
+	
+	public String deleteBook(String isbn) {
+		if(bookRepository.existsById(isbn)) {
+			bookRepository.deleteById(isbn);
+			return "Book Deleted";
+		}else {
+			throw new BookNotFoundException("Book Not Found");
+		}
+	}
+	
+	public Book updateBook(Book book) {
+		if(bookRepository.existsById(book.getIsbn())) {
+			return bookRepository.save(book);
+		}else {
+			throw new BookNotFoundException("Book Not Found");
 		}
 	}
 
