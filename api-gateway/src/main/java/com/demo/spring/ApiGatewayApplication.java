6abc351c222ff4ac0c0db5edcb2392d08cb1a594
sbtalk71 <sbtalk@yahoo.com>
2025-03-27 13:05:12 +0530

    api-gateway commit 2

--- api-gateway/src/main/java/com/demo/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/demo/spring/ApiGatewayApplication.java
@@ -2,12 +2,19 @@ package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@GetMapping("/library-fallback")
+	public String message() {
+		return "Library Service not available";
+	}
 }
