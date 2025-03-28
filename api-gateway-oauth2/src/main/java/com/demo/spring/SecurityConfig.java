7d237ff339e73a1c3faab951387b027a53335e6d
sbtalk71 <sbtalk@yahoo.com>
2025-03-28 12:04:16 +0530

    oauth2 config done

--- api-gateway-oauth2/src/main/java/com/demo/spring/SecurityConfig.java
+++ api-gateway-oauth2/src/main/java/com/demo/spring/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.demo.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,33 +7,21 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.demo.spring.jwt.util.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	@Autowired
-	JwtFilter jwtFilter;
+	
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.csrf(csrf->csrf.disable());
 		http.authorizeHttpRequests(
-				requests->requests.requestMatchers("/library/**").hasRole("USER")
-				.requestMatchers("/books/**").hasRole("ADMIN").anyRequest().permitAll());
+				requests->requests.requestMatchers("/redirect/**").permitAll().anyRequest().authenticated());
 		
-		http.exceptionHandling(Customizer.withDefaults())
-		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-		//http.httpBasic(Customizer.withDefaults());
+		http.oauth2Login(Customizer.withDefaults());
 		
 		return http.build();
 	}
